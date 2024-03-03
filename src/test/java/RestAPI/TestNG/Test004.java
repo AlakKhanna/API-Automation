43aa50a6ddc999b34edae0b6083e15cdc1308d03
AlakKhanna <alak.khanna@gmail.com>
2024-03-03 23:54:42 +0530

    End to end automation

---
+++ src/test/java/RestAPI/TestNG/Test004.java
@@ -0,0 +1,20 @@
+package RestAPI.TestNG;
+
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test004 {
+
+    @BeforeTest
+    public  void t1(){
+        System.out.println("1");
+    }
+    @BeforeTest
+    public  void t2(){
+        System.out.println("2");
+    }
+    @Test
+    public  void t3(){
+        System.out.println("3");
+    }
+}
---
+++ src/test/java/RestAPI/TestNG/Test005.java
@@ -0,0 +1,105 @@
+package RestAPI.TestNG;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matchers;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class Test005 {
+
+    String token;
+
+    int bookingid;
+
+    RequestSpecification requestSpecification;
+
+    ValidatableResponse validatableResponse;
+
+    Response response;
+
+
+
+    @BeforeTest
+    public  void getToken(){
+        System.out.println("Get token");
+        requestSpecification = RestAssured.given();
+        String payload = "{\n" +
+                "        \"username\": \"admin\",\n" +
+                "        \"password\": \"password123\"\n" +
+                "}";
+        requestSpecification.baseUri("https://restful-booker.herokuapp.com");
+        requestSpecification.basePath("auth");
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.body(payload);
+        Response response = requestSpecification.post();
+        validatableResponse= response.then();
+        validatableResponse.body("token", Matchers.notNullValue());
+        token = response.then().log().all().extract().path("token");
+        System.out.println(token);
+    }
+    @BeforeTest
+    public  void GetBookingID(){
+        System.out.println("Get booking ID-");
+        String payload = "{\n" +
+                "    \"firstname\" : \"Amit\",\n" +
+                "    \"lastname\" : \"Brown\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : true,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2018-01-01\",\n" +
+                "        \"checkout\" : \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Breakfast\"\n" +
+                "}";
+
+        requestSpecification = RestAssured.given();
+        requestSpecification.baseUri("https://restful-booker.herokuapp.com");
+        requestSpecification.basePath("booking");
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.body(payload);
+
+        response = requestSpecification.when().post();
+
+        validatableResponse= response.then().log().all();
+        validatableResponse.statusCode(200);
+
+        bookingid= response.then().log().all().extract().path("bookingid");
+        System.out.println(bookingid);
+    }
+    @Test
+    public  void testPutRequest(){
+        System.out.println("Put request");
+
+        String payload = "{\n" +
+                "    \"firstname\" : \"Pramod\",\n" +
+                "    \"lastname\" : \"Brown\",\n" +
+                "    \"totalprice\" : 111,\n" +
+                "    \"depositpaid\" : true,\n" +
+                "    \"bookingdates\" : {\n" +
+                "        \"checkin\" : \"2018-01-01\",\n" +
+                "        \"checkout\" : \"2019-01-01\"\n" +
+                "    },\n" +
+                "    \"additionalneeds\" : \"Breakfast\"\n" +
+                "}";
+
+        requestSpecification = RestAssured.given();
+        requestSpecification.baseUri("https://restful-booker.herokuapp.com");
+        requestSpecification.basePath("booking/" + bookingid);
+        requestSpecification.contentType(ContentType.JSON);
+        requestSpecification.cookie("token", token);
+        requestSpecification.body(payload);
+
+        response = requestSpecification.when().put();
+
+        validatableResponse= response.then().log().all();
+        validatableResponse.statusCode(200);
+        validatableResponse.body("firstname", Matchers.equalTo("Pramod"));
+        validatableResponse.body("lastname", Matchers.equalTo("Brown"));
+
+
+    }
+}
